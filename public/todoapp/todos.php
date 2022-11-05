c5f8bad2d29968d12053282952ecaa908201a934
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2022-11-05 11:18:34 +0000

    Show as table with bootstrap CSS.

--- public/todoapp/todos.php
+++ public/todoapp/todos.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
 <body>
 <?php
 $servername = "localhost";
@@ -16,16 +19,26 @@ if ($conn->connect_error) {
 
 $sql = "SELECT id, todo FROM todo";
 $result = $conn->query($sql);
+?>
+<div class="container">
 
+<?php
 if ($result->num_rows > 0) {
+  echo '<table class="table">
+  <tr>
+    <th>ID</th>
+    <th>Item</th>
+  </tr>';
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Todo: " . $row["todo"]. "<br>";
+    echo "<tr><td>" . $row["id"]. "</td><td>" . $row["todo"]. "</td></tr>";
   }
+  echo '</table>';
 } else {
-  echo "0 results";
+  echo "<h1>0 results</h1>";
 }
 $conn->close();
 ?>
+</div>
 </body>
 </html>
